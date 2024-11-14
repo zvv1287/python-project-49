dfb00aeda58aa2df850d46026d11f2c7455c6a59
zvv <zvv1287@yandex.ru>
2024-11-14 19:54:14 +0300

    Внесены константы на пределы значений в калькулятор

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -2,6 +2,9 @@ import brain_games.cli
 from brain_games.games.communication_with_users import user_answer, check_user_ansver_and_print_res
 import random
 
+LOWER_LIMIT = 1
+UPPER_LIMIT = 10
+
 
 def calc():
     user_name = brain_games.cli.welcome_user()
@@ -9,7 +12,8 @@ def calc():
     i = 0
     flag = True
     while i < 3 and flag:
-        number_1, number_2 = random.randint(1, 10), random.randint(1, 10)
+        number_1, number_2 = (random.randint(LOWER_LIMIT, UPPER_LIMIT),
+                              random.randint(LOWER_LIMIT, UPPER_LIMIT))
         sign = random.choice(['+', '-', '*'])
         match sign:
             case '+':
@@ -22,3 +26,4 @@ def calc():
         answer = user_answer(f'Question: {number_1} {sign} {number_2}')
         flag = check_user_ansver_and_print_res(user_name, result, answer)
         i += 1
+
